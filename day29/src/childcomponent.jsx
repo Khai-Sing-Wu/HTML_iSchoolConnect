4131507244f3fb847f18ae728c3a522f43c0dd2b
Khai-Sing-Wu <86360549+Khai-Sing-Wu@users.noreply.github.com>
2022-04-11 13:55:03 +0530

    Add files via upload

---
+++ day29/src/childcomponent.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import PropTypes from 'prop-types'
+ 
+class ChildComp extends Component{
+   /*  static defaultProps = {
+        title : 'default child title',
+        version : 10
+    } */
+    static propTypes = {
+        title : PropTypes.string.isRequired ,
+        version : PropTypes.number.isRequired
+    }
+    render(){
+        return <div>
+                    {/*  
+                    <h3>Title : { this.props.title ||  'default child title'}</h3>
+                    <h3>Version : { this.props.version || 5 }</h3> 
+                    */}
+
+                    <h3>Title : { this.props.title }</h3>
+                    <h3>Version : { this.props.version }</h3>
+                </div>
+    }
+}
+ 
+export default ChildComp	
+ 
